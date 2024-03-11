0ec5804b46ee0b7051e98dd7618b3c6daac6c4d6
tupsutumppu <61348006+Tumppi66@users.noreply.github.com>
2024-03-11 20:46:29 +0200

    Delete Mike Cash Bypasser directory

--- Mike Cash Bypasser/source.lua
+++
@@ -1,9 +0,0 @@
-local oldHttpGet
-oldHttpGet = hookfunction(game.HttpGet, newcclosure(function(...)
-    local args = {...}
-
-    if args[2] and string.find(tostring(args[2]), "/v2/tokens/exists/") then
-        return game:GetService("HttpService"):JSONEncode({success = true})
-    end
-    return oldHttpGet(...)
-end))
